74db3ed7f6e32aa9cd786130cbc227d487d68296
duddel <duddel@users.noreply.github.com>
2025-02-23 19:03:32 +0100

    fix default mod_dir

--- mod_filecopy.bat
+++ mod_filecopy.bat
@@ -4,6 +4,7 @@ REM This script is called as a post-build event in VS and
 REM copies the binaries (.dll) to the half-life installation.
 
 REM Adjust mod_dir to your half-life installation + mod subdir
-set mod_dir=E:\SteamLibrary\steamapps\common\Half-Life\hllab\
+REM Example: E:\SteamLibrary\steamapps\common\Half-Life\hllab\
+set mod_dir=_deploy\hllab\
 
 xcopy %1 %mod_dir%%2% /Y
